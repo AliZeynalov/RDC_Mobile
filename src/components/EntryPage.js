f8baa3f364fcce0f142f892611715756b756b1a3
Ali Zeynalov <ali.yasharoglu@gmail.com>
2017-09-05 17:42:22 +0300

    Entry Page functionality done.

--- src/components/EntryPage.js
+++ src/components/EntryPage.js
@@ -1,6 +1,6 @@
 import React, {Component} from 'react';
-import {Dimensions, AsyncStorage,Picker,Alert} from 'react-native';
-import {Container, Form, H3, Header, Footer, Button, Text, Spinner,Card,Left,Body,Content,ListItem, List, Radio,View, Thumbnail} from 'native-base';
+import {Dimensions, AsyncStorage,Picker,Alert,TouchableOpacity} from 'react-native';
+import {Container, Form, H3, Header, Footer, Button, Text, Spinner,Card,Left,Right, Body,Content,ListItem, List, Radio,View, Thumbnail} from 'native-base';
 import {Actions} from 'react-native-router-flux';
 
 const {width, height} = Dimensions.get("window"),
@@ -12,15 +12,29 @@ export default class EntryPage extends Component {
     constructor(props) {
         super(props);
         this.state = {
+            users: []
         }
 
     }
 
 
-
-
-    componentWillMount()
+    getDetails()
     {
+        var Url = "https://vast-brushlands-78701.herokuapp.com/test/All";
+        return fetch(Url, {method: "GET"})
+            .then((response) => response.json())
+            .then((responseJson) => {
+            console.log("GIRDI");
+                this.setState({users: responseJson},()=>console.log("user state",this.state.users));
+            })
+            .done();
+    }
+
+
+
+    componentDidMount()
+    {
+        this.getDetails();
     }
 
 
@@ -36,23 +50,26 @@ export default class EntryPage extends Component {
                 <Header style={{backgroundColor: 'red'}}/>
                 <Content>
                     <List>
-                        <ListItem>
-                            <Thumbnail square size={80} source={{ uri: 'Image URL' }} />
-                            <Body>
-                            <Text>Sankhadeep</Text>
-                            <Text note>Its time to build a difference . .</Text>
-                            </Body>
-                        </ListItem>
-                        <ListItem>
-                            <Thumbnail square size={80} source={{ uri: 'Image URL' }} />
-                            <Body>
-                            <Text>Sankhadeep</Text>
-                            <Text note>Its time to work</Text>
-                            </Body>
-                        </ListItem>
+                        {this.state.users.map((user,i)=>{
+                            return(
+                                <ListItem key={i}>
+                                    <Thumbnail square size={80} source={{ uri: 'Image URL' }} />
+                                    <Body>
+                                    <Text>{user.name}</Text>
+                                    <Text note>{user.description}</Text>
+                                    </Body>
+                                    <Right>
+                                        <TouchableOpacity><Text>View</Text></TouchableOpacity>
+                                    </Right>
+                                </ListItem>
+                            );
+                        })}
                     </List>
                 </Content>
             </Container>
         );
     }
 }
+
+
+
